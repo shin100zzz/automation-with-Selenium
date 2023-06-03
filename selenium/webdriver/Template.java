f18084b674524a99e5feb29827f86b844b49fd53
shin100zzz <nhnghia2406@gmail.com>
2023-06-03 15:01:15 +0700

    test

--- selenium/webdriver/Template.java
+++ selenium/webdriver/Template.java
@@ -1,43 +1,27 @@
 package webdriver;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.openqa.selenium.JavascriptExecutor;
 
 public class Template {
-	
+
 	WebDriver driver;
 	Actions action;
 	WebDriverWait expliciWait;
 	JavascriptExecutor jsExecutor;
 	long timeinsecond = 2;
-	
+
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
-	
-	
+
 	@BeforeClass
 	public void beforeClass() {
 		if (osName.contains("Windows")) {
@@ -47,7 +31,7 @@ public class Template {
 			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
 //			System.setProperty("webdriver.chrome.driver",projectPath + "/browserDrivers/chromedriver");
 		}
-		
+
 		driver = new FirefoxDriver();
 //		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
@@ -57,21 +41,21 @@ public class Template {
 
 	@Test
 	void test() {
-		
+		// test
+		driver.findElement(null);
 	}
 
 	@AfterClass
 	static void afterClass() throws Exception {
-	
+
 	}
-	
+
 	public void sleepinSeconds(long timeoutinsecond) {
 		try {
-			Thread.sleep(timeoutinsecond*1000);
+			Thread.sleep(timeoutinsecond * 1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 }
-	
